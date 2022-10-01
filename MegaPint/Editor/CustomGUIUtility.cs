a6e323ea2a0dc8437bfede260309998811bbd53a
Tiogiras <tiogiras@gmail.com>
2022-10-01 02:23:43 +0200

    Added MegaPint

---
+++ MegaPint/Editor/CustomGUIUtility.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MegaPint.Editor {
+    public static class CustomGUIUtility {
+        
+        public static GUIStyle Headline(int size, Color color) {
+            var style = Headline(size);
+            style.normal.textColor = color;
+            return style;
+        }
+        
+        public static GUIStyle Headline(int size) {
+            var style = new GUIStyle {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = size,
+                fontStyle = FontStyle.Bold,
+                normal = {textColor = Color.white}
+            };
+            return style;
+        }
+
+        public static GUIStyle CenteredText(int size, Color color) {
+            var style = CenteredText(size);
+            style.normal.textColor = color;
+            return style;
+        }
+        
+        public static GUIStyle CenteredText( int size ) {
+            var style = new GUIStyle {
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = size,
+                normal = {textColor = new Color( .75f, .75f, .75f, 1 ) }
+            };
+            return style;
+        }
+        
+        public static void GuiLine( int height ) {
+             EditorGUILayout.Separator(  );
+             var rect = EditorGUILayout.GetControlRect(false, height );
+             rect.height = height;
+             EditorGUI.DrawRect(rect, new Color ( 0.15f,0.15f,0.15f, 1 ) );
+             EditorGUILayout.Separator(  );
+         }
+    }
+}
---
+++ MegaPint/Editor/Utility/AutoSaveWindow.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace MegaPint.Editor.Utility {
+    public class AutoSaveWindow : EditorWindow {
+        
+        private static MegaPintSettingsData _settingsData;
+        
+        private DateTime _lastSaved;
+        private DateTime _nextSave;
+        private float _nextInterval;
+
+        [MenuItem("MegaPint/Utility/AutoSave", false, 11)]
+        private static void Init() {
+            var window = GetWindow(typeof(AutoSaveWindow));
+            window.Show();
+        }
+
+        private void OnEnable() {
+            _settingsData = MegaPintSettingsData.CheckForExistingDataAsset();
+            SetNextTime();
+        }
+
+        private void OnGUI() {
+            if (_settingsData == null) {
+                EditorGUILayout.HelpBox("This feature requires an existing data asset!", MessageType.Error);
+                return;
+            }
+
+            if (Application.isPlaying) {
+                EditorGUILayout.HelpBox("Disabled during play mode.", MessageType.Warning);
+                return;
+            }
+
+            EditorGUILayout.LabelField("Last saved:", _lastSaved.ToLongTimeString());
+        
+            var timeToSave = (int)(_nextInterval - EditorApplication.timeSinceStartup);
+            EditorGUILayout.LabelField("Next Save:", _nextSave.ToLongTimeString() + " (" + (timeToSave + " Sec)") );
+            Repaint();
+
+            EditorGUILayout.BeginHorizontal();
+        
+            EditorGUILayout.LabelField("Current Interval:", _settingsData.intervalTime + " Sec");
+
+            if (GUILayout.Button("Change")) ChangeIntervalWindow.InitWindow();
+        
+            EditorGUILayout.EndHorizontal();
+
+            EditorUtility.SetDirty(this);
+        
+            if (!(EditorApplication.timeSinceStartup > _nextInterval)) return;
+
+            EditorSceneManager.SaveOpenScenes();
+            Debug.Log( _lastSaved + ": MegaPint Scene AutoSave");
+            SetNextTime();
+        }
+
+        private void SetNextTime() {            
+            _lastSaved = DateTime.Now;
+            _nextSave = DateTime.Now.AddSeconds(_settingsData.intervalTime + 1);
+            _nextInterval = (int)(EditorApplication.timeSinceStartup + _settingsData.intervalTime);
+        }
+
+        private class ChangeIntervalWindow : EditorWindow {
+            public static void InitWindow() {
+                var windowRect = new Rect(new Vector2(0, 0), new Vector2(400, 100));
+                var window = GetWindowWithRect(typeof(ChangeIntervalWindow), windowRect, true, "Change TimeInterval");
+                window.Show();
+            }
+
+            private void OnGUI() {
+                if (_settingsData == null) {
+                    EditorGUILayout.HelpBox("This feature requires an existing data asset!", MessageType.Error);
+                    return;
+                }
+                
+                EditorGUILayout.HelpBox("The TimeInterval sets the time(sec) after which the opened scene is saved.", MessageType.Info);
+                _settingsData.intervalTime = EditorGUILayout.IntField("Current TimeInterval", _settingsData.intervalTime);
+                EditorUtility.SetDirty(_settingsData);
+            }
+        }
+
+        private void OnDestroy() {
+            if (_settingsData == null) return;
+            EditorApplication.Beep();
+            Debug.LogWarning("MegaPint AutoSave disabled!");
+        }
+    }
+}
