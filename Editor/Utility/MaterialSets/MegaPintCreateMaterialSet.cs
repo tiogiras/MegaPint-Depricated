d6372f62f17e9c94d6d94af2558a4f80c50568cd
Tiogiras <tiogiras@gmail.com>
2023-01-09 14:24:40 +0100

    Hotfixes

--- Editor/Utility/MaterialSets/MegaPintCreateMaterialSet.cs
+++ Editor/Utility/MaterialSets/MegaPintCreateMaterialSet.cs
@@ -8,12 +8,12 @@ using UnityEngine;
 namespace MegaPint.Editor.Utility.MaterialSets {
     public class MegaPintCreateMaterialSet : EditorWindow {
 
-        private static List<Material> _materials;
-        private static string _setName;
-        private static Vector2 _scrollPos;
+        private static List<Material> Materials;
+        private static string SetName;
+        private static Vector2 ScrollPos;
 
-        private static bool _nameValidated;
-        private static bool _nameValid;
+        private static bool NameValidated;
+        private static bool NameValid;
 
         public static void Init() {
             MegaPint.CreateMaterialSetWindow = GetWindow(typeof(MegaPintCreateMaterialSet), true, "Create Material Set");
@@ -23,12 +23,12 @@ namespace MegaPint.Editor.Utility.MaterialSets {
 
             MegaPintMaterialSets.UpdateMaterialSetsTemp();
             
-            _materials = new List<Material>();
-            _setName = "";
-            _scrollPos = Vector2.zero;
+            Materials = new List<Material>();
+            SetName = "";
+            ScrollPos = Vector2.zero;
 
-            _nameValidated = false;
-            _nameValid = false;
+            NameValidated = false;
+            NameValid = false;
         }
 
         private void OnGUI() {
@@ -40,22 +40,22 @@ namespace MegaPint.Editor.Utility.MaterialSets {
             EditorGUILayout.Separator();
 
             EditorGUILayout.BeginHorizontal();
-            _setName = EditorGUILayout.TextField("Name", _setName);
-            EditorGUILayout.LabelField(_nameValidated ? _nameValid ? "Valid" : "Not valid" : "", GUILayout.Width(60));
+            SetName = EditorGUILayout.TextField("Name", SetName);
+            EditorGUILayout.LabelField(NameValidated ? NameValid ? "Valid" : "Not valid" : "", GUILayout.Width(60));
             if (GUILayout.Button("Validate", GUILayout.Width(60))) {
-                _nameValid = ValidateName();
-                _nameValidated = true;
+                NameValid = ValidateName();
+                NameValidated = true;
             }
             EditorGUILayout.EndHorizontal();
             
-            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUIStyle.none, GUI.skin.verticalScrollbar, GUILayout.ExpandWidth(true), GUILayout.Height(175));
-            if (_materials.Count > 0) {
-                for (var i = 0; i < _materials.Count; i++) {
+            ScrollPos = EditorGUILayout.BeginScrollView(ScrollPos, GUIStyle.none, GUI.skin.verticalScrollbar, GUILayout.ExpandWidth(true), GUILayout.Height(175));
+            if (Materials.Count > 0) {
+                for (var i = 0; i < Materials.Count; i++) {
                     EditorGUILayout.Separator();
                     EditorGUILayout.BeginHorizontal();
-                        _materials[i] = (Material)EditorGUILayout.ObjectField("Slot " + (i + 1), _materials[i], typeof(Material), false);
+                        Materials[i] = (Material)EditorGUILayout.ObjectField("Slot " + (i + 1), Materials[i], typeof(Material), false);
                         if (GUILayout.Button("Remove", MegaPint.MegaPintGUI.GetStyle("button1"), GUILayout.MaxWidth(75))) {
-                            _materials.RemoveAt(i);
+                            Materials.RemoveAt(i);
                         }
                     EditorGUILayout.EndHorizontal();
                 }
@@ -66,16 +66,16 @@ namespace MegaPint.Editor.Utility.MaterialSets {
             EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.Separator(); EditorGUILayout.Separator();
                 if (GUILayout.Button("Add Material Slot", MegaPint.MegaPintGUI.GetStyle("button1"), GUILayout.Height(25), GUILayout.Width(125))) {
-                    _materials.Add(null);
+                    Materials.Add(null);
                 }
             EditorGUILayout.EndHorizontal();
             
             EditorGUILayout.Separator();
-            if (_materials.Count <= 0) EditorGUILayout.HelpBox("The set has no materials!", MessageType.Warning);
-            else if (_materials.Any(material => material == null)) EditorGUILayout.HelpBox("A material slot is missing a material!", MessageType.Warning);
-            else if (_setName.Equals("")) EditorGUILayout.HelpBox("The set needs a name!", MessageType.Warning);
-            else if (!_nameValidated) EditorGUILayout.HelpBox("Validate the name before saving!", MessageType.Warning);
-            else if (!_nameValid) EditorGUILayout.HelpBox("There already exists a set with this name! (Validate to check)", MessageType.Warning);
+            if (Materials.Count <= 0) EditorGUILayout.HelpBox("The set has no materials!", MessageType.Warning);
+            else if (Materials.Any(material => material == null)) EditorGUILayout.HelpBox("A material slot is missing a material!", MessageType.Warning);
+            else if (SetName.Equals("")) EditorGUILayout.HelpBox("The set needs a name!", MessageType.Warning);
+            else if (!NameValidated) EditorGUILayout.HelpBox("Validate the name before saving!", MessageType.Warning);
+            else if (!NameValid) EditorGUILayout.HelpBox("There already exists a set with this name! (Validate to check)", MessageType.Warning);
             else {
                 if (GUILayout.Button("Create Material Set", MegaPint.MegaPintGUI.GetStyle("button1"))) {
                     CreateSetAsset();
@@ -87,10 +87,10 @@ namespace MegaPint.Editor.Utility.MaterialSets {
 
         private void CreateSetAsset() {
             var asset = CreateInstance<MegaPintMaterialSet>();
-            asset.materialSetName = _setName;
-            asset.materials = _materials;
+            asset.materialSetName = SetName;
+            asset.materials = Materials;
 
-            var path = MegaPint.Settings.materialSetsSavePath + "/" + _setName + ".asset";
+            var path = MegaPint.Settings.materialSetsSavePath + "/" + SetName + ".asset";
             AssetDatabase.CreateAsset(asset, path);
             asset.assetPath = path;
             
@@ -102,9 +102,9 @@ namespace MegaPint.Editor.Utility.MaterialSets {
         }
 
         private static bool ValidateName() {
-            if (_setName.Equals("")) return false;
-            if (_setName.StartsWith(" ")) return false;
-            return MegaPint.Settings.materialSets.Count <= 0 || MegaPint.Settings.materialSets.All(set => !set.materialSetName.Equals(_setName));
+            if (SetName.Equals("")) return false;
+            if (SetName.StartsWith(" ")) return false;
+            return MegaPint.Settings.materialSets.Count <= 0 || MegaPint.Settings.materialSets.All(set => !set.materialSetName.Equals(SetName));
         }
     }
 }
--- Editor/Utility/MaterialSets/MegaPintMaterialSets.cs
+++ Editor/Utility/MaterialSets/MegaPintMaterialSets.cs
@@ -1,12 +1,24 @@
 ﻿#if UNITY_EDITOR
 
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace MegaPint.Editor.Utility.MaterialSets {
-    public class MegaPintMaterialSets {
+    public static class MegaPintMaterialSets {
         public static void UpdateMaterialSetsTemp() {
-            var found = Resources.LoadAll("MaterialSets", typeof(MegaPintMaterialSet)).Cast<MegaPintMaterialSet>().ToList();
+            var paths = Directory.GetFiles(MegaPint.Settings.materialSetsSavePath, "*.asset");
+
+            var found = new List<MegaPintMaterialSet>();
+            foreach (var path in paths) {
+                var obj = AssetDatabase.LoadAssetAtPath<MegaPintMaterialSet>(path);
+                if (obj == null) continue;
+                found.Add(obj);
+            }
+            
+            Debug.Log($"found {found.Count} objects");
             MegaPint.Settings.materialSets = found;
             MegaPint.Settings.materialSetsFoldouts = new bool[found.Count].ToList();
         }
