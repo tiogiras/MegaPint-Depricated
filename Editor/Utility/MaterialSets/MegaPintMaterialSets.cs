0de9176c94c2d982e6679a4ee9c7b6e250457627
Tiogiras <tiogiras@gmail.com>
2023-01-09 14:29:26 +0100

    Some CleanUp

--- Editor/Utility/MaterialSets/MegaPintMaterialSets.cs
+++ Editor/Utility/MaterialSets/MegaPintMaterialSets.cs
@@ -1,24 +1,14 @@
 ﻿#if UNITY_EDITOR
 
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 
 namespace MegaPint.Editor.Utility.MaterialSets {
     public static class MegaPintMaterialSets {
         public static void UpdateMaterialSetsTemp() {
             var paths = Directory.GetFiles(MegaPint.Settings.materialSetsSavePath, "*.asset");
-
-            var found = new List<MegaPintMaterialSet>();
-            foreach (var path in paths) {
-                var obj = AssetDatabase.LoadAssetAtPath<MegaPintMaterialSet>(path);
-                if (obj == null) continue;
-                found.Add(obj);
-            }
-            
-            Debug.Log($"found {found.Count} objects");
+            var found = paths.Select(AssetDatabase.LoadAssetAtPath<MegaPintMaterialSet>).Where(obj => obj != null).ToList();
             MegaPint.Settings.materialSets = found;
             MegaPint.Settings.materialSetsFoldouts = new bool[found.Count].ToList();
         }
